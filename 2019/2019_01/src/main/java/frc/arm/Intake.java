f458594faa00252ca65077431cf6fad4594f5248
Danny Yoo <elitenick57@gmail.com>
2019-02-09 15:20:11 -0800

    asdf

---
+++ 2019/2019_01/src/main/java/frc/arm/Intake.java
@@ -0,0 +1,22 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.arm;
+
+import edu.wpi.first.wpilibj.Spark;
+
+/**
+ * Add your docs here.
+ */
+public class Intake {
+    Spark left;
+    Spark right;
+
+    public void init(){
+
+    }
+}
